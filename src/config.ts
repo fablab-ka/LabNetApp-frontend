cc8dcab501882a049a3b5d48d15d0cd553900d0f
Sven <sven.hecht@gmail.com>
2018-09-12 18:35:50 +0200

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
     serverUrl: 'http://192.168.1.6:3333/',
     // serverUrl: 'http://localhost:3333/',
-    socketServerUrl: 'http://localhost:3333/'
+    socketServerUrl: 'http://192.168.1.6:3333/'
 };
